3210f7588e5a6ca80afb8b1b038faf5fabdecd5e
Paul Willis <ios_dev@paulrobertwillis.com>
2021-06-30 12:26:25 +0100

    Added visual score; added correction on incorrect flag choice;  added final score in each round

--- Project02/ViewController.swift
+++ Project02/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     
     var countries = [String]()
     var score = 0
+    var correctFlagNumber = 0
+    var questionsAsked = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,14 +29,62 @@ class ViewController: UIViewController {
         button1.layer.borderColor = UIColor.lightGray.cgColor
         button2.layer.borderColor = UIColor.lightGray.cgColor
         button3.layer.borderColor = UIColor.lightGray.cgColor
-
+        
         askQuestion()
     }
 
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
+        countries.shuffle()
+        correctFlagNumber = Int.random(in: 0...2)
+        
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        title = countries[correctFlagNumber].uppercased() + " | Score: \(score)"
+    }
+    
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        questionsAsked += 1
+        
+        if sender.tag == correctFlagNumber {
+            score += 1
+            validateAnswer(correct: true, sender: sender)
+        } else {
+            validateAnswer(correct: false, sender: sender)
+        }
+    }
+    
+    func validateAnswer(correct: Bool, sender: UIButton) {
+        
+        if questionsAsked == 10 {
+            let ac = UIAlertController(title: "Round Ended", message: "Your final score is \(score) out of \(questionsAsked)", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Finish", style: .default, handler: askQuestion))
+            present(ac, animated: true)
+            score = 0
+            questionsAsked = 0
+        } else if correct == true {
+            correctAnswer()
+        } else {
+            wrongAnswer(sender: sender)
+        }
+    }
+    
+    func correctAnswer() {
+        let ac = UIAlertController(title: "Correct", message: "Your score is \(score)", preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        
+        present(ac, animated: true)
+    }
+    
+    func wrongAnswer(sender: UIButton) {
+        let ac = UIAlertController(title: "Incorrect", message: "This is the flag of: \n\(countries[sender.tag].uppercased())", preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        
+        present(ac, animated: true)
     }
 }
 
